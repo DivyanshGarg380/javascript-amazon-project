d2ecd151a77019aa7b11a77a98304f02411e72eb
Divyansh Garg <HIMYNAMEISDIVYANSH@GMAIL.COM>
2025-07-24 00:33:18 +0530

    make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart,removeFromCart,updateQuantity} from '../data/cart.js';
+import {cart,removeFromCart,updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from "./utils/money.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
@@ -60,7 +60,7 @@ cart.forEach((item) => {
         </div>
         <div class = "delivery-options">
              <div class = "delivery-options-title">
-                 Choose a delivery Option " 
+                 Choose a delivery Option : 
         </div>
         ${deliveryOptionHTML(matchingProduct,item)}
             </div>
@@ -84,7 +84,9 @@ function deliveryOptionHTML(matchingProduct,item){
     
 
         html+=
-        ` <div class="delivery-option">
+        ` <div class="delivery-option js-delivery-option"
+        data-product-id = "${matchingProduct.id}"
+        data-delivery-option-id = "${deliveryOption.id}">
             <input type="radio"
             ${isChecked ? 'checked' : ''}
             class="delivery-option-input"
@@ -119,4 +121,11 @@ document.querySelectorAll('.js-add-quantity').forEach((link)=>{
         const productId = link.dataset.productId;
         updateQuantity(productId);
     });
+});
+
+document.querySelectorAll('.js-delivery-option').forEach((element)=>{
+    element.addEventListener('click',() =>{
+        const {productId,deliveryOptionId} = element.dataset;
+        updateDeliveryOption(productId,deliveryOptionId);
+    })
 });
